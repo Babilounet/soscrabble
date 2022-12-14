963c19d59be324d0ee51ef9d3b54d0624f3f768f
Babilounet <babilounet@gmail.com>
2022-12-15 00:16:06 +0100

    value letters

--- js/ScrabbleTools.class.js
+++ js/ScrabbleTools.class.js
@@ -3,6 +3,19 @@ import {ScrabbleBoard} from "./ScrabbleBoard.class.js";
 
 export class ScrabbleTools {
 
+    // Tableau officiel
+    // const aScores = {
+    //     0: '?',
+    //     1: 'a,e,i,l,n,o,r,s,t,u',
+    //     2: 'd,g',
+    //     3: 'b,c,m,p',
+    //     4: 'f,h,v,w,y',
+    //     5: 'k',
+    //     8: 'j,x',
+    //     10: 'q,z'
+    // };
+
+
     /**
      * Search the score value of the given letter
      * @param {string} sLetter
@@ -11,13 +24,13 @@ export class ScrabbleTools {
     static getScoreByLetter(sLetter) {
         const aScores = {
             0: '?',
-            1: 'a,e,i,l,n,o,r,s,t,u',
-            2: 'd,g',
-            3: 'b,c,m,p',
-            4: 'f,h,v,w,y',
-            5: 'k',
-            8: 'j,x',
-            10: 'q,z'
+            1: 'a,e,i,n,o,r,s,t',
+            2: 'l,u',
+            3: 'd',
+            4: 'c,m,p',
+            5: 'h,f,g,b',
+            8: 'v,z',
+            10: 'y,w,q,k,j,x'
         };
         if (sLetter === ' ') {
             return 0;
